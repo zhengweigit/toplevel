f196958bbbd4cbddb9809ea38cdc392a8aeaa475
zhengweigit <zhengwei_git@163.com>
2016-06-27 11:41:43 +0800

    第一次使用

--- app/src/main/java/com/phone1000/administrator/mygit_leveltopapplication/MainActivity.java
+++ app/src/main/java/com/phone1000/administrator/mygit_leveltopapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.phone1000.administrator.mygit_leveltopapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //git第一次使用
+        TextView tvgit = (TextView) findViewById(R.id.tv_git);
+        tvgit.setText("you are SB,do you know?");
     }
 }
